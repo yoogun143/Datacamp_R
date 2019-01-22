e4308e830c56b5ecd3292b87be4a7d4a9cf39ef2
yoogun143 <thanhhl95@gmail.com>
2019-01-21 21:32:33 -0500

    1/21/19

--- Dplyr Manipulation/2.2. MUTATE.R
+++
@@ -1,8 +0,0 @@
-library(hflights)
-library(dplyr)
-
-# Add the three variables as described in the third instruction: m2
-m2 <- mutate(hflights, 
-             TotalTaxi = TaxiIn + TaxiOut, 
-             ActualGroundTime = ActualElapsedTime - AirTime, 
-             Diff = TotalTaxi - ActualGroundTime)
